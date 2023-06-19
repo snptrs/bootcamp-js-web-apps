cd4167f92d43aa185646e0a9622e6291c826de31
Sean Peters <sean@snptrs.dev>
2023-06-19 13:32:33 +0100

    Passing test for .addNotes

--- 1-setting-up/3-notes-app/notesModel.test.js
+++ 1-setting-up/3-notes-app/notesModel.test.js
@@ -1,10 +1,18 @@
 const NotesModel = require("./notesModel");
 
-describe("", () => {
+describe("NotesModel", () => {
   describe(".getNotes", () => {
     it("returns all notes", () => {
       const model = new NotesModel();
       expect(model.getNotes()).toEqual([]);
+    });
+  });
+  describe(".addNotes", () => {
+    it("adds a note with the given text", () => {
+      const model = new NotesModel();
+      model.addNote("Buy milk");
+      model.addNote("Go to the gym");
+      expect(model.getNotes()).toEqual(["Buy milk", "Go to the gym"]);
     });
   });
 });
