6f9d4cae7392441f884aee2c9829a2ce3860200e
Sean Peters <sean@snptrs.dev>
2023-06-19 13:34:40 +0100

    Passing test for .reset; completes 1.4.1

--- 1-setting-up/3-notes-app/notesModel.test.js
+++ 1-setting-up/3-notes-app/notesModel.test.js
@@ -7,12 +7,22 @@ describe("NotesModel", () => {
       expect(model.getNotes()).toEqual([]);
     });
   });
+
   describe(".addNotes", () => {
     it("adds a note with the given text", () => {
       const model = new NotesModel();
       model.addNote("Buy milk");
       model.addNote("Go to the gym");
       expect(model.getNotes()).toEqual(["Buy milk", "Go to the gym"]);
+    });
+  });
+
+  describe(".reset", () => {
+    it("clears all notes", () => {
+      const model = new NotesModel();
+      model.addNote("Buy milk");
+      model.reset();
+      expect(model.getNotes()).toEqual([]);
     });
   });
 });
