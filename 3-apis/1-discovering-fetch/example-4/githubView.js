9403685194e8e3f0e0cdd5f6e88f594d8229cbb9
Sean Peters <sean@snptrs.dev>
2023-06-20 12:01:11 +0100

    Completed 3.1 (12)

--- 3-apis/1-discovering-fetch/example-4/githubView.js
+++ 3-apis/1-discovering-fetch/example-4/githubView.js
@@ -3,20 +3,28 @@ class GithubView {
     this.model = model;
     this.client = client;
 
-    const submitButtonEl = document.querySelector('#submit-button');
-    const repoInputEl = document.querySelector('#repo-name-input');
+    const submitButtonEl = document.querySelector("#submit-button");
+    const repoInputEl = document.querySelector("#repo-name-input");
 
-    submitButtonEl.addEventListener('click', () => {
+    submitButtonEl.addEventListener("click", () => {
       const repoName = repoInputEl.value;
 
-      this.client.getRepoInfo(repoName, repoData => {
+      this.client.getRepoInfo(repoName, (repoData) => {
         console.log(repoData);
+        this.display(repoData);
       });
     });
   }
 
-  display() {
+  display(repoData) {
+    const repoName = document.querySelector("#repo-name");
+    repoName.textContent = repoData.full_name;
 
+    const repoDescription = document.querySelector("#repo-description");
+    repoDescription.textContent = repoData.description;
+
+    const repoImage = document.querySelector("#repo-image");
+    repoImage.src = repoData.organization.avatar_url;
   }
 }
 
