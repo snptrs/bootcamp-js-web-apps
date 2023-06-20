69e2177095ebc00decb4f337567933d64c7b2d47
Sean Peters <sean@snptrs.dev>
2023-06-20 08:20:24 +0100

    Completes 2.4 (8)

--- 2-interactivity/4-handling-user-interaction/example-3/messageView.js
+++ 2-interactivity/4-handling-user-interaction/example-3/messageView.js
@@ -1,19 +1,29 @@
 class MessageView {
   constructor() {
     this.buttonEl = document.querySelector("#show-message-button");
-
     this.buttonEl.addEventListener("click", () => {
       this.displayMessage();
+    });
+
+    this.removeButtonEl = document.querySelector("#hide-message-button");
+    this.removeButtonEl.addEventListener("click", () => {
+      this.removeMessage();
     });
   }
 
   displayMessage() {
-    console.log("Thanks for clicking me!");
     const container = document.querySelector("#main-container");
     const div = document.createElement("div");
     div.id = "message";
     div.textContent = "This message displayed by JavaScript";
     container.append(div);
+  }
+
+  removeMessage() {
+    const messages = document.querySelectorAll("#message");
+    messages.forEach((el) => {
+      el.remove();
+    });
   }
 }
 
--- 2-interactivity/4-handling-user-interaction/example-3/messageView.test.js
+++ 2-interactivity/4-handling-user-interaction/example-3/messageView.test.js
@@ -2,18 +2,32 @@
  * @jest-environment jsdom
  */
 
-const fs = require('fs');
-const MessageView = require('./messageView');
+const fs = require("fs");
+const MessageView = require("./messageView");
 
-describe('MessageView', () => {
-  it('clicks the button', () => {
-    document.body.innerHTML = fs.readFileSync('./index.html');
+describe("MessageView", () => {
+  it("adds a message", () => {
+    document.body.innerHTML = fs.readFileSync("./index.html");
 
     const view = new MessageView();
 
-    const buttonEl = document.querySelector('#show-message-button');
+    const buttonEl = document.querySelector("#show-message-button");
     buttonEl.click();
 
-    expect(document.querySelector('#message')).not.toBeNull();
+    expect(document.querySelector("#message")).not.toBeNull();
+  });
+
+  it("removes a message", () => {
+    document.body.innerHTML = fs.readFileSync("./index.html");
+
+    const view = new MessageView();
+
+    const addButtonEl = document.querySelector("#show-message-button");
+    addButtonEl.click();
+    expect(document.querySelector("#message")).not.toBeNull();
+
+    const removeButtonEl = document.querySelector("#hide-message-button");
+    removeButtonEl.click();
+    expect(document.querySelector("#message")).toBeNull();
   });
 });
